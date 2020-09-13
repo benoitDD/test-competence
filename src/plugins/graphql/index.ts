f934eed5ce477ff1fa487a87f9cdd33814daef8d
benoitDD <benoitdegorsas@outlook.fr>
2020-09-13 10:58:57 +0200

    use fastify graphql

--- src/plugins/graphql/index.ts
+++ src/plugins/graphql/index.ts
@@ -1,26 +1,27 @@
 import { FastifyPluginAsync } from 'fastify'
 import fastifyPlugin from 'fastify-plugin'
-import { schema as schemaString } from './schema'
-import { graphql, buildSchema } from 'graphql'
-import { root } from './root'
-
-declare module 'fastify' {
-    interface FastifyInstance {
-        graphql: {
-            execute: (query: string) => ReturnType<typeof graphql>
-        }
-    }
-}
+//import { schema as schemaString } from './schema'
+//import { root } from './root'
+import GQL from 'fastify-gql'
 
 const graphqlPlugins: FastifyPluginAsync = async function (app) {
-    const schema = buildSchema(schemaString)
+    //const schema = buildSchema(schemaString)
 
-    function execute(query: string) {
-        return graphql(schema, query, root)
+    const schema = `
+    type Query {
+        add(x: Int, y: Int): Int
+    }
+    `
+    const add: (nada: unknown, args: { x: number; y: number }) => Promise<number> = async (_, { x, y }) => x + y
+    const resolvers = {
+        Query: {
+            add: add,
+        },
     }
 
-    app.decorate('graphql', {
-        execute,
+    app.register(GQL, {
+        schema,
+        resolvers,
     })
 }
 
--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,10 +1,8 @@
 import { FastifyPluginAsync } from 'fastify'
 import healthcheckRoute from './healthcheck'
-import graphqlRoute from './graphql'
 
 const routes: FastifyPluginAsync = async function (app) {
     app.register(healthcheckRoute)
-    app.register(graphqlRoute)
 }
 
 export default routes
