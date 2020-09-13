4d0629386f0213461e9901b9fd4b88d47b2c6548
benoitDD <benoitdegorsas@outlook.fr>
2020-09-13 14:55:59 +0200

    graphql

--- src/plugins/graphql/index.ts
+++ src/plugins/graphql/index.ts
@@ -1,12 +1,10 @@
 import { FastifyPluginAsync } from 'fastify'
 import fastifyPlugin from 'fastify-plugin'
-//import { schema as schemaString } from './schema'
+//import { schema } from './schema'
 //import { root } from './root'
 import GQL from 'fastify-gql'
 
 const graphqlPlugins: FastifyPluginAsync = async function (app) {
-    //const schema = buildSchema(schemaString)
-
     const schema = `
     type Query {
         add(x: Int, y: Int): Int
